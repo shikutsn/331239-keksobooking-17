336abe255e2375436bbb8947b5e183dafba2f826
shikutsn <46595890+shikutsn@users.noreply.github.com>
2019-07-10 01:15:31 +0300

    процесс идет

--- js/map.js
+++ js/map.js
@@ -2,19 +2,28 @@
 
 (function () {
   var PinPointerOffset = {
-    X: 25,
-    Y: 70
+    X: -25,
+    Y: -70
   };
   var MainPinPointerOffset = {
-    X: 31,
-    Y: 42
+    X: -31,
+    Y: -84 // проверить 80 или 84. Думаю, что 84 (картинка 62 и стрелка еще 22)
   };
 
+
+  var getAdjustedPinCoords = function (coord) {
+    var result = coord;
+    result.x += PinPointerOffset.X;
+    result.y += PinPointerOffset.Y;
+    return result;
+  };
 
   var renderPin = function (pin, template) {
     var pinEl = template.cloneNode(true);
+    var adjustedCoords = getAdjustedPinCoords(pin.location);
 
-    pinEl.style.cssText = 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px;';
+    pinEl.style.left = adjustedCoords.x + 'px';
+    pinEl.style.top = adjustedCoords.y + 'px';
     pinEl.src = pin.author.avatar;
     pinEl.alt = 'заголовок объявления';
 
@@ -32,7 +41,6 @@
   };
 
   var renderMapPins = function (pins) {
-    // пока что нет поправки на ширину пинов
     var pinTemplate = document.querySelector('#pin')
       .content
       .querySelector('.map__pin');
@@ -42,8 +50,48 @@
     pinsEl.appendChild(fragment);
   };
 
+  var mapEl = document.querySelector('.map');
+  var adFormEl = document.querySelector('.ad-form');
+  var adFormFieldsetsEls = adFormEl.querySelectorAll('fieldset');
+  var filtersFormEl = document.querySelector('.map__filters');
+  var filtersFormSelectEls = filtersFormEl.querySelectorAll('select');
+  var filtersFormFieldsetEls = filtersFormEl.querySelectorAll('fieldset');
+
+  // больно много в этих двух функциях однотипного кода. Упростить?
+  // типа мапы, которая будет подменять add на remove?
+  var setMapInactive = function () {
+    mapEl.classList.add('map--faded');
+    adFormEl.classList.add('ad-form--disabled');
+    adFormFieldsetsEls.forEach(function (it) {
+      it.disabled = true;
+    });
+    filtersFormSelectEls.forEach(function (it) {
+      it.disabled = true;
+    });
+    filtersFormFieldsetEls.forEach(function (it) {
+      it.disabled = true;
+    });
+  };
+
+  // почему эллипс вокруг главного пина не уменьшается до 62х62?
+  var setMapActive = function () {
+    mapEl.classList.remove('map--faded');
+    adFormEl.classList.remove('ad-form--disabled');
+    adFormFieldsetsEls.forEach(function (it) {
+      it.disabled = false;
+    });
+    filtersFormSelectEls.forEach(function (it) {
+      it.disabled = false;
+    });
+    filtersFormFieldsetEls.forEach(function (it) {
+      it.disabled = false;
+    });
+  };
+
 
   window.map = {
-    renderMapPins: renderMapPins
+    renderMapPins: renderMapPins,
+    setMapActive: setMapActive,
+    setMapInactive: setMapInactive
   };
 })();
