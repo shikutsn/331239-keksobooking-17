99968e71e931140bb1a2f31f5359fd98161dbf04
Shikuts Nikita <shikutsn@bk.ru>
2019-08-02 00:51:36 +0300

    причесан модуль карт

--- js/map.js
+++ js/map.js
@@ -2,16 +2,25 @@
 
 (function () {
   // TODO: пройтись по замечаниям Вадима из readme.md от 29.07. Поискать подобные случаи и их фикс
-  // TODO а вообще, интересная тема - вынести функции по переключению режимов страницы в отдельный модуль
-  // TODO и другая интересная тема - манипуляции с mainPin тоже вынести в отдельный модуль
   var PinData = {
     MAX_COUNT: 5,
     SIZE: {
       WIDTH: 50,
       HEIGHT: 70
+    }
+  };
+  var MainPinData = {
+    SIZE: {
+      ACTIVE: {
+        WIDTH: 65,
+        HEIGHT: 65
+      },
+      INACTIVE: {
+        WIDTH: 65,
+        HEIGHT: 84
+      }
     },
-    // TODO перекинуть эти ограничения в MainPinSize. И обозвать MainPinData
-    COORD: {
+    COORD_LIMITS: {
       ABSCISS: {
         MIN: 0,
         MAX: 1200
@@ -20,36 +29,13 @@
         MIN: 130,
         MAX: 630
       }
-    }
-  };
-  var MainPinSize = {
-    MAP_ACTIVE: {
-      WIDTH: 65,
-      HEIGHT: 65
     },
-    MAP_INACTIVE: {
-      WIDTH: 65,
-      HEIGHT: 84
-    }
+    DEFAULT_STYLE: 'left: 570px; top: 375px;'
   };
-  var MAIN_PIN_DEFAULT_STYLE = 'left: 570px; top: 375px;';
-  // TODO эти константы надо переделать, логика работы функции (раз)блокировки карты сильно эволюционировала
-  // var BlockStates = {
-  //   ACTIVE: 'active',
-  //   INACTIVE: 'inactive'
-  // };
-  // var BlockStatesMap = {
-  //   'active': {
-  //     styleAction: 'remove',
-  //     elementsAction: window.util.enableElement
-  //   },
-  //   'inactive': {
-  //     styleAction: 'add',
-  //     elementsAction: window.util.disableElement
-  //   }
-  // };
-  var DISABLED_MAP_CLS = 'map--faded';
-  var DISABLED_AD_FORM_CLS = 'ad-form--disabled';
+  var DisabledCls = {
+    MAP: 'map--faded',
+    AD_FORM: 'ad-form--disabled'
+  };
 
   var renderPin = function (pin, template) {
     var pinEl = template.cloneNode(true);
@@ -99,25 +85,25 @@
   var mainPinEl = mapEl.querySelector('.map__pin--main');
 
   var setFiltersFormActive = function (filtersFormState) {
-    if (filtersFormState) {
-      filtersFormInnerEls.forEach(window.util.enableElement);
-    } else {
-      filtersFormInnerEls.forEach(window.util.disableElement);
-    }
+    return filtersFormState ? filtersFormInnerEls.forEach(window.util.enableElement) : filtersFormInnerEls.forEach(window.util.disableElement);
   };
 
   var setMapActive = function (mapState) {
-    if (mapState) {
-      mapEl.classList.remove(DISABLED_MAP_CLS);
-      adFormEl.classList.remove(DISABLED_AD_FORM_CLS);
+    var activateMap = function () {
+      mapEl.classList.remove(DisabledCls.MAP);
+      adFormEl.classList.remove(DisabledCls.AD_FORM);
       adFormInnerEls.forEach(window.util.enableElement);
       window.validation.init();
-    } else {
-      mapEl.classList.add(DISABLED_MAP_CLS);
-      adFormEl.classList.add(DISABLED_AD_FORM_CLS);
+    };
+
+    var deactivateMap = function () {
+      mapEl.classList.add(DisabledCls.MAP);
+      adFormEl.classList.add(DisabledCls.AD_FORM);
       adFormInnerEls.forEach(window.util.disableElement);
       window.data.reset();
-    }
+    };
+
+    return mapState ? activateMap() : deactivateMap();
   };
 
   var onLoadingSuccess = function (pins) {
@@ -127,17 +113,16 @@
   };
 
   var fillAddressField = function (addressFieldEl, pinEl, isMapActive) {
-    var addressX = isMapActive ? pinEl.offsetLeft + MainPinSize.MAP_ACTIVE.WIDTH / 2 : pinEl.offsetLeft + MainPinSize.MAP_INACTIVE.WIDTH / 2;
-    var addressY = isMapActive ? pinEl.offsetTop + MainPinSize.MAP_ACTIVE.HEIGHT : pinEl.offsetTop + MainPinSize.MAP_INACTIVE.HEIGHT;
+    var addressX = isMapActive ? pinEl.offsetLeft + MainPinData.SIZE.ACTIVE.WIDTH / 2 : pinEl.offsetLeft + MainPinData.SIZE.INACTIVE.WIDTH / 2;
+    var addressY = isMapActive ? pinEl.offsetTop + MainPinData.SIZE.ACTIVE.HEIGHT : pinEl.offsetTop + MainPinData.SIZE.INACTIVE.HEIGHT;
     addressFieldEl.value = Math.round(addressX) + ', ' + addressY;
   };
 
   var resetMainPin = function () {
-    mainPinEl.style = MAIN_PIN_DEFAULT_STYLE;
+    mainPinEl.style = MainPinData.DEFAULT_STYLE;
     fillAddressField(addressEl, mainPinEl, false);
   };
 
-  // ---------------------------------------
   var Rect = function (left, top, right, bottom) {
     this.left = left;
     this.top = top;
@@ -145,13 +130,18 @@
     this.bottom = bottom;
   };
 
-  var Coordinate = function (x, y, constraints) {
+  var Coordinate = function (x, y) {
     this.x = x;
     this.y = y;
-    this._constraints = constraints;
   };
 
-  Coordinate.prototype.setCoord = function (x, y) {
+  var PinCoordinate = function (x, y, constraints) {
+    Coordinate.call(this, x, y);
+    this._constraints = constraints;
+  };
+  PinCoordinate.prototype = Object.create(Coordinate.prototype);
+
+  PinCoordinate.prototype.setCoord = function (x, y) {
     var setSingleCoord = function (coord, minValue, maxValue) {
       if (coord < minValue) {
         return minValue;
@@ -166,68 +156,27 @@
     this.y = setSingleCoord(y, this._constraints.top, this._constraints.bottom);
   };
 
-  var coordRect = new Rect(PinData.COORD.ABSCISS.MIN, PinData.COORD.ORDINATE.MIN, PinData.COORD.ABSCISS.MAX - MainPinSize.MAP_ACTIVE.WIDTH, PinData.COORD.ORDINATE.MAX);
-  // ---------------------------------------
-
-  var getSingleCoord = function (coord, minValue, maxValue) {
-    if (coord < minValue) {
-      return minValue;
-    }
-    if (coord > maxValue) {
-      return maxValue;
-    }
-    return coord;
-  };
+  var coordRect = new Rect(MainPinData.COORD_LIMITS.ABSCISS.MIN - Math.round(MainPinData.SIZE.ACTIVE.WIDTH / 2), MainPinData.COORD_LIMITS.ORDINATE.MIN, MainPinData.COORD_LIMITS.ABSCISS.MAX - Math.round(MainPinData.SIZE.ACTIVE.WIDTH / 2), MainPinData.COORD_LIMITS.ORDINATE.MAX);
 
   mainPinEl.addEventListener('mousedown', function (evt) {
-    if (mapEl.classList.contains(DISABLED_MAP_CLS)) {
+    if (mapEl.classList.contains(DisabledCls.MAP)) {
       setMapActive(true);
     }
-    var startCoord = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-    var startPinCoord = {
-      x: mainPinEl.offsetLeft,
-      y: mainPinEl.offsetTop
-    };
+    var startCoord = new Coordinate(evt.clientX, evt.clientY);
+    var pinStartCoord = new Coordinate(mainPinEl.offsetLeft, mainPinEl.offsetTop);
 
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
 
-      // if (moveEvt.clientX >= PinData.COORD.ABSCISS.MIN && moveEvt.clientX <= PinData.COORD.ABSCISS.MAX - MainPinSize.MAP_ACTIVE.WIDTH && moveEvt.clientY >= PinData.COORD.ORDINATE.MIN && moveEvt.clientY <= PinData.COORD.ORDINATE.MAX) {
-      var shiftCoord = {
-        x: moveEvt.clientX - startCoord.x,
-        y: moveEvt.clientY - startCoord.y
-      };
+      var shiftCoord = new Coordinate(moveEvt.clientX - startCoord.x, moveEvt.clientY - startCoord.y);
 
-      var newPinCoord = {
-        x: getSingleCoord(startPinCoord.x + shiftCoord.x, PinData.COORD.ABSCISS.MIN, PinData.COORD.ABSCISS.MAX - MainPinSize.MAP_ACTIVE.WIDTH),
-        y: getSingleCoord(startPinCoord.y + shiftCoord.y, PinData.COORD.ORDINATE.MIN, PinData.COORD.ORDINATE.MAX)
-        // x: startPinCoord.x + shiftCoord.x,
-        // y: startPinCoord.y + shiftCoord.y
-      };
+      var newPinCoord = new PinCoordinate(0, 0, coordRect);
+      newPinCoord.setCoord(pinStartCoord.x + shiftCoord.x, pinStartCoord.y + shiftCoord.y);
+
       mainPinEl.style.left = newPinCoord.x + 'px';
       mainPinEl.style.top = newPinCoord.y + 'px';
 
       fillAddressField(addressEl, mainPinEl, true);
-
-
-      // TODO упростить расчеты координат
-      // TODO объект boundaries?
-      // см лекцию 8, 50мин. Там про объекты, конструкторы, методы прототипов. Мб стоит демку там же посмотреть, даже точно стоит!
-      // TODO а вообще, двигается плохо - если мышка двигается из-за границы карты, то пин ползает, не будучи привязанным к курсору мыши. Так же было в кекстаграме
-      // newCoord.x = getSingleCoord(newCoord.x, PinData.COORD.ABSCISS.MIN, PinData.COORD.ABSCISS.MAX - MainPinSize.MAP_ACTIVE.WIDTH);
-      // newCoord.y = getSingleCoord(newCoord.y, PinData.COORD.ORDINATE.MIN, PinData.COORD.ORDINATE.MAX);
-      // TODO: cделать так, чтобы поведение перетаскивания метки было как в фильтре кекстаграмма
-      // запоминаем ""коорд мыши"", запоминаем коорд пина
-      // на маузмуве:
-      // записываем координаты смещения (shiftCoord)
-      // от коорд пина отнимаем коорд смещения
-      // перезаписываем ""коорд мыши""
-      // меняем координаты пина (стили): если меньше ограничителя снизу, то равно ему, если больше огр сверху, то равно ему
-      // иначе равно координатам пина
-
     };
 
     var onMouseUp = function (upEvt) {
